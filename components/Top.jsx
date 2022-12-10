8117fd8d51bfff51a68663e623fe610f1ecac79f
joycehwchan <joycehwchan@gmail.com>
2022-12-10 22:31:49 +0900

    added top component

---
+++ components/Top.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Top.module.css'
+
+const Top = () => {
+  return (
+    <div className={styles.top}>
+      <h1>Slice of Japan</h1>
+      <h3>Share your life in Japan</h3>
+    </div>
+  )
+}
+
+export default Top
